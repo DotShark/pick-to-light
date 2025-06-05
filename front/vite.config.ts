128c86cfc29bf362183b555a6c99bc009b0cec3a
Sol1de <olivier.delmas@edu.devinci.fr>
2025-06-05 11:59:02 +0200

    :sparkles: added & fixed paths custom

--- front/vite.config.ts
+++ front/vite.config.ts
@@ -14,7 +14,15 @@ export default defineConfig({
   ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    },
-  },
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+      '@assets': fileURLToPath(new URL('./src/assets', import.meta.url)),
+      '@components': fileURLToPath(new URL('./src/components', import.meta.url)),
+      '@layouts': fileURLToPath(new URL('./src/layouts', import.meta.url)),
+      '@router': fileURLToPath(new URL('./src/router', import.meta.url)),
+      '@store': fileURLToPath(new URL('./src/store', import.meta.url)),
+      '@utils': fileURLToPath(new URL('./src/utils', import.meta.url)),
+      '@views': fileURLToPath(new URL('./src/views', import.meta.url)),
+      '@icons': fileURLToPath(new URL('./src/components/icons', import.meta.url))
+    }
+  }
 })
