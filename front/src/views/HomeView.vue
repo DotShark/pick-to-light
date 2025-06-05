d80edc6f06464dd87403c6956a0502a1ceafb104
Sol1de <olivier.delmas@edu.devinci.fr>
2025-06-05 12:19:23 +0200

    :sparkles: refacto

--- front/src/views/HomeView.vue
+++ front/src/views/HomeView.vue
@@ -1,5 +1,16 @@
 <script setup lang="ts">
+import { onMounted } from 'vue'
+import { useRouter } from 'vue-router'
 import TheWelcome from '@components/TheWelcome.vue'
+
+const router = useRouter()
+
+onMounted(() => {
+  const token = localStorage.getItem('token')
+  if (!token) {
+    router.push('/login')
+  }
+})
 </script>
 
 <template>
---
+++ front/src/views/LoginView.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import LoginForm from '@components/Login.vue'
+</script>
+
+<template>
+  <main>
+    <LoginForm />
+  </main>
+</template>
