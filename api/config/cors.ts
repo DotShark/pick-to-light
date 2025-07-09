d792a4a5510d108796b3f257aa67e9b74227d085
Sol1de <olivier.delmas@edu.devinci.fr>
2025-07-09 17:52:11 +0200

    :sparkles: cors allowed all corss origin

--- api/config/cors.ts
+++ api/config/cors.ts
@@ -8,7 +8,7 @@ import { defineConfig } from '@adonisjs/cors'
  */
 const corsConfig = defineConfig({
   enabled: true,
-origin: ['http://localhost:3000', 'http://localhost:5173', 'http://localhost:8080', 'http://127.0.0.1:3000', 'http://127.0.0.1:5173', 'http://127.0.0.1:8080', 'http://localhost:5174'],
+  origin: true, // Autoriser toutes les origines
   methods: ['GET', 'HEAD', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
   headers: ['Content-Type', 'Authorization', 'X-Requested-With'],
   exposeHeaders: [],
